afc8bfb0e9c70183ccf9c299822de3462f4aa7df
Louminator <louisfrossi@gmail.com>
2024-01-21 13:14:26 -0500

    Moving collector code back to the correct lineage of our competition code.

---
+++ src/main/java/frc/robot/commands/setCollectorCommand.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.headSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class setCollectorCommand extends Command {
+   //as soon as sensor trips, stop collector.
+    //collectingFromSourceState; neutralState (stowing away the note);
+   // shootingPositionComannd, collcetingOffGround, depositAmp, depositTrap
+    private final headSubsystem headSubsystem;
+    private final double speed;
+
+
+
+    public setCollectorCommand(headSubsystem headSubsystem, double speed) {
+        this.headSubsystem = headSubsystem;
+        this.speed = speed;
+        addRequirements(headSubsystem);
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {
+     headSubsystem.setCollectorSpeed(speed, speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
