a0ffd39e9b786c8d5720f4037b6ffcc93f20b22b
RCode2 <rohan.sivva@gmail.com>
2024-01-25 18:53:45 -0500

    Method stubs added to work on.

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,4 +1,26 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+
+import java.util.List;
+
 public class Vision {
+
+    Pose2d odometryPosition;
+
+    public void setOdometryPosition(Pose2d pose){ //Rohan
+        odometryPosition = pose;
+    }
+
+    public Pose2d getRobotPosition(){// Tanmaybe
+        return null;
+    }
+
+    public void setCameraPosition(){ //Rohan
+
+    }
+
+    public List<Pose2d> objectDetection(){ //Both
+        return null;
+    }
 }
