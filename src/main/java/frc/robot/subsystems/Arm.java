38e9758e7eae67ecedf497457d400f7b60f7cdb9
malti <maltimjohn1@gmail.com>
2024-02-01 20:34:11 -0500

    Arm code fully written and I am happy with it. Now, I'm going to find a good intermediary point using my graph.

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,19 +7,12 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.*;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.helpers.AutoCodeLines;
+import frc.robot.commands.ArmPathFollow;
 
-import org.opencv.core.Point;
-import java.util.ArrayList;
 import static com.revrobotics.CANSparkLowLevel.MotorType.kBrushless;
 
 public class Arm extends SubsystemBase {
@@ -32,43 +25,33 @@ public class Arm extends SubsystemBase {
     private final PIDController shoulderController;
     private final PIDController wristController;
 
-    private Timer timer;
-    private Point intermediaryPoint;
-    private boolean finSpot = false;
-    private boolean armInPos = false;
-    private Point desPose, startPose;
+    private Rotation2d safeWrist, safeShoulder;
+    private double maxSpeed, maxAccel;
 
-    double maxArmSpeed, targetDist;
-//TODO: do path stuff in separate command
     public Arm(int shoulderMotorID, int wristMotorID, int shoulderEncoderID, int wristEncoderID,
                double kPShoulder, double kIShoulder, double kDShoulder,
                double kPWrist, double kIWrist, double kDWrist,
-               double maxArmSpeed, Rotation2d safeShoulderAngle, Rotation2d safeWristAngle) {
+               Rotation2d safeShoulderAngle, Rotation2d safeWristAngle,
+               double maxSpeed, double maxAccel) {
+
         shoulderMotor = new CANSparkMax(shoulderMotorID, kBrushless);
         wristMotor = new CANSparkMax(wristMotorID, kBrushless);
 
         shoulderEncoder = new CANcoder(shoulderEncoderID);
         wristEncoder = new CANcoder(wristEncoderID);
 
+        this.maxAccel = maxAccel; this.maxSpeed = maxSpeed;
 
         shoulderController = new PIDController(kPShoulder, kIShoulder, kDShoulder);
         wristController = new PIDController(kPWrist, kIWrist, kDWrist);
-
-        this.maxArmSpeed = maxArmSpeed;
-
-        intermediaryPoint = new Point(safeShoulderAngle.getDegrees(), safeWristAngle.getDegrees());
-        finSpot = true;
-        desPose = getArmState();
-        timer = new Timer();
-        armInPos = true;
+        safeShoulder = safeShoulderAngle; safeWrist = safeWristAngle;
     }
 
     public void periodic(){
         //TODO: make the mechanism 2d object in here
-        wayPointFollow(timer.get());
     }
 
-    private void pathFollow(Rotation2d shoulder, Rotation2d wrist){
+    public void pathFollow(Rotation2d shoulder, Rotation2d wrist){
         //x is shoulder, y is wrist
         shoulderController.setSetpoint(shoulder.getDegrees());
         shoulderMotor.set(shoulderController.calculate(shoulderState().getDegrees()));
@@ -76,71 +59,26 @@ public class Arm extends SubsystemBase {
         wristMotor.set(wristController.calculate(wristState().getDegrees()));
     }
 
+    public Command goToPoint(Rotation2d shoulderPos, Rotation2d wristPos) {
 
-    Rotation2d shoulderState(){
+        /*
+        Add fun logic in here to find the good intermediary point
+        */
+
+        return new SequentialCommandGroup(
+                new ArmPathFollow(this, safeShoulder, safeWrist, maxSpeed, maxAccel),
+                new ArmPathFollow(this, shoulderPos, wristPos, maxSpeed, maxAccel)
+        );
+    }
+
+
+    public Rotation2d shoulderState(){
         return Rotation2d.fromDegrees(shoulderEncoder.getAbsolutePosition().getValueAsDouble());
     }
 
-    Rotation2d wristState(){
+    public Rotation2d wristState(){
         return Rotation2d.fromDegrees(wristEncoder.getAbsolutePosition().getValueAsDouble());
     }
-
-    private Point getArmState(){
-        return new Point(shoulderState().getDegrees(), wristState().getDegrees());
-    }
-
-    public void goToPoint(Rotation2d shoulderPose, Rotation2d wristPose){
-        desPose = new Point(shoulderPose.getDegrees(), shoulderPose.getDegrees());
-        startPose = getArmState();
-        double xDel = startPose.x-intermediaryPoint.x;
-        double yDel = startPose.y-intermediaryPoint.y;
-        targetDist = Math.sqrt(xDel*xDel+yDel*yDel);
-        finSpot = false;
-        armInPos = false;
-    }
-
-    private void wayPointFollow(double time){
-
-        double s = AutoCodeLines.getS(targetDist,.2, maxArmSpeed/2, time);
-        double shoulderPos, wristPos;
-
-        if (!finSpot){
-            shoulderPos = AutoCodeLines.getPositionX(startPose, intermediaryPoint, s);
-            wristPos = AutoCodeLines.getPositionY(startPose, intermediaryPoint, s);
-            double deltX = (intermediaryPoint.x-getArmState().x);
-            double deltY = (intermediaryPoint.y-getArmState().y);
-            double delt = Math.sqrt(deltY*deltY+deltX*deltX);
-            if (delt <= .1){
-                timer.restart();
-                finSpot = true;
-                double xDel = desPose.x-getArmState().x;
-                double yDel = desPose.y-getArmState().y;
-                targetDist = Math.sqrt(xDel*xDel+yDel*yDel);
-            }
-        }
-        else if (!armInPos){
-            shoulderPos = AutoCodeLines.getPositionX(intermediaryPoint, desPose, s);
-            wristPos = AutoCodeLines.getPositionY(intermediaryPoint, desPose, s);
-            double deltX = (desPose.x-getArmState().x);
-            double deltY = (desPose.y-getArmState().y);
-            double delt = Math.sqrt(deltY*deltY+deltX*deltX);
-            //TODO: log everything
-            if (delt <= .1){
-                shoulderPos = desPose.x; wristPos = desPose.y;
-                armInPos = true;
-            }
-        }
-        SmartDashboard.putNumber("shoulderPos", shoulderPos);
-        SmartDashboard.putNumber("wristPos", wristPos);
-        pathFollow(Rotation2d.fromDegrees(shoulderPos), Rotation2d.fromDegrees(wristPos));
-    }
-
-    public boolean armInPos(){
-        return this.armInPos;
-    }
-
-
-
 
 
 }
