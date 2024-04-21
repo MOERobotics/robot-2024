15bb5316f063cc22161842c873cb6ad6f8f911be
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-20 22:47:13 -0500

    Add AdvantageKit dep

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,16 +4,14 @@
 
 package frc.robot;
 
-import com.pathplanner.lib.pathfinding.Pathfinding;
-import com.pathplanner.lib.pathfinding.LocalADStar;
+import org.littletonrobotics.junction.LoggedRobot;
+
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.SwerveDrive;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 //
   private FortissiMOEContainer m_robotContainer; //= new FortissiMOEContainer();;
