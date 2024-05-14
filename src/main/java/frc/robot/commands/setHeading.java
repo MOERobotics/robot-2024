bf34bf804a5aedd4e1124b8b5897124ba446109e
anjali <anjaliyermalkumar@gmail.com>
2024-05-13 20:07:57 -0400

    updated auto aim

--- src/main/java/frc/robot/commands/setHeading.java
+++ src/main/java/frc/robot/commands/setHeading.java
@@ -3,11 +3,8 @@ package frc.robot.commands;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.internal.DriverStationModeThread;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.SwerveDrive;
 
 import java.util.function.Supplier;
