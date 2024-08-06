4536eb51587bfb7f70deeeeec0e1a1677519fd51
Luca Huang <luca.hym@gmail.com>
2024-08-05 21:36:30 -0400

    Changed KrakenMotor so setInvert does not reset other configs, pending testing.

--- src/main/java/frc/robot/subsystems/KrakenMotor.java
+++ src/main/java/frc/robot/subsystems/KrakenMotor.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-//import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
@@ -14,21 +12,20 @@ public class KrakenMotor extends SubsystemBase implements GenericMotor{
 	private final TalonFX motor;
 	private VelocityVoltage driveVelocityVoltage;
 	private double motorFF;
+	private final TalonFXConfiguration motorConfig = new TalonFXConfiguration();
 	public KrakenMotor(int motorID) {
 		this.motor = new TalonFX(motorID);
-		TalonFXConfiguration MotorConfig = new TalonFXConfiguration();
-		MotorConfig.CurrentLimits.withStatorCurrentLimitEnable(true);
-		MotorConfig.CurrentLimits.withStatorCurrentLimit(60);
-		motor.getConfigurator().apply(MotorConfig);
+		motorConfig.CurrentLimits.withStatorCurrentLimitEnable(true);
+		motorConfig.CurrentLimits.withStatorCurrentLimit(60);
+		motor.getConfigurator().apply(motorConfig);
 		motor.setNeutralMode(NeutralModeValue.Brake);
 	}
 	public KrakenMotor(int motorID, boolean invert, double motorP, double motorI, double motorD, double motorFF) {
 		this.motor = new TalonFX(motorID);
-		TalonFXConfiguration MotorConfig = new TalonFXConfiguration();
-		MotorConfig.CurrentLimits.withStatorCurrentLimitEnable(true);
-		MotorConfig.CurrentLimits.withStatorCurrentLimit(60);
-		MotorConfig.MotorOutput.Inverted = invert?InvertedValue.Clockwise_Positive:InvertedValue.CounterClockwise_Positive;
-		motor.getConfigurator().apply(MotorConfig);
+		motorConfig.CurrentLimits.withStatorCurrentLimitEnable(true);
+		motorConfig.CurrentLimits.withStatorCurrentLimit(60);
+		motorConfig.MotorOutput.Inverted = invert?InvertedValue.Clockwise_Positive:InvertedValue.CounterClockwise_Positive;
+		motor.getConfigurator().apply(motorConfig);
 		motor.setNeutralMode(NeutralModeValue.Brake);
 		Slot0Configs PIDConfigs = new Slot0Configs();
 		PIDConfigs.kP = motorP;
@@ -41,24 +38,19 @@ public class KrakenMotor extends SubsystemBase implements GenericMotor{
 
 	@Override
 	public void setPID(double motorP, double motorI, double motorD, double motorFF){
-		Slot0Configs PIDConfigs = new Slot0Configs();
-		PIDConfigs.kP = motorP;
-		PIDConfigs.kI = motorI;
-		PIDConfigs.kD = motorD;
-		motor.getConfigurator().apply(PIDConfigs);
+		Slot0Configs pidConfigs = new Slot0Configs();
+		motor.getConfigurator().refresh(pidConfigs);
+		pidConfigs.kP = motorP;
+		pidConfigs.kI = motorI;
+		pidConfigs.kD = motorD;
+		motor.getConfigurator().apply(pidConfigs);
 		driveVelocityVoltage = new VelocityVoltage(0).withSlot(0);
 		this.motorFF = motorFF;
 	}
 
 	@Override
 	public void setInvert(boolean invert) {
-		motor.getConfigurator().apply(
-				new TalonFXConfiguration().withMotorOutput(
-						new MotorOutputConfigs().withInverted(
-								invert?InvertedValue.Clockwise_Positive:InvertedValue.CounterClockwise_Positive
-						)
-				)
-		);
+		motor.setInverted(invert);
 	}
 
 	@Override
